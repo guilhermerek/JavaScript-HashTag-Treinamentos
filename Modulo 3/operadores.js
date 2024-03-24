a8b2bbe87e58a8cd7844c7aa6736a06d5250622d
Guilherme <50538146+guilhermerek@users.noreply.github.com>
2024-03-24 16:02:23 -0300

    Novos codigos
    ajuste de pasta e novos codigos referente a aula

---
+++ Modulo 3/operadores.js
@@ -0,0 +1,27 @@
+const resultadoMultiplicacao = 7*3;
+const resultadoDivisao = 10/2;
+const resultadoExpoenciacao = 2**3;
+const resultadoRestoDivisao = 29%7;
+let resultadoIncremento = 2;
+let resultadoDecremento = 3;
+resultadoIncremento++;
+resultadoDecremento--;
+
+console.log(resultadoMultiplicacao)
+console.log(resultadoDivisao)
+console.log(resultadoExpoenciacao)
+console.log(resultadoRestoDivisao)
+console.log(resultadoIncremento)
+console.log(resultadoDecremento)
+
+const texto1 = "Olá, Meu Nome é: Fulano de Tal! ";
+const texto2 = "E tenho 1 gato!"
+const textoSoma = texto1 + texto2;
+console.log(textoSoma)
+
+
+
+const um = 1;
+const dois = 2;
+const resultado = um - dois;
+console.log(resultado)
---
+++ Modulo 3/variaveis.js
@@ -0,0 +1,16 @@
+//como funciona uma variavel no JS
+
+const mensagem = "Olá, Mundo! Esta é uma constante.";
+//const: constante, não altera o valor!
+
+let mensagemNaVariavel = "Olá, Mundo! Esta é uma variavel.";
+//let é o novo var, permite mudar o valor!!
+
+
+console.log(mensagem);
+console.log(mensagemNaVariavel);
+
+mensagemNaVariavel = "Agora perceba! O quarto texto irá se alterar, pois fiz a mudança na frase!";
+
+console.log(mensagem);
+console.log(mensagemNaVariavel);
