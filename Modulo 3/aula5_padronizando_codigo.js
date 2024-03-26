83247bec17f7d5b948921f8056d3c0eb0c43cac6
Guilherme <50538146+guilhermerek@users.noreply.github.com>
2024-03-25 22:43:02 -0300

    Aulas
    Aula sobre padronização e tipo de dados comum no JS

---
+++ Modulo 3/aula5_padronizando_codigo.js
@@ -0,0 +1,16 @@
+// let permite inicializar sem valor
+//const precisa inicializar e definir um valor
+
+let variavelUm = 'Primeiro';
+let variavelDois;
+const variavelTres = 'Terceiro';
+variavelDois = 'Segundo';
+
+console.log(variavelUm);
+console.log(variavelDois);
+console.log(variavelTres);
+
+// ponto e virgula é utilizado por padronização, já que outras linguagens utilizam
+// o JS reconhece o fim da instrução, porém ";" facilita a leitura tbm
+
+//com o prettier instalado, um auxilio na formatação, usar: ALT+SHIFT+F
---
+++ Modulo 3/tipoDeDados.js
@@ -0,0 +1,10 @@
+//String aquilo que já conhecemos, usando '' ou ""
+//String Template, usamos `` (crase), string com que permite expressoes embutidas em si
+
+const num = 99;
+
+const stringTemplate = `Um número de exmplo de uma expressão: ${num}`;
+
+console.log(stringTemplate);
+
+// no JS int e float é entendido como tipo Numbers, não usa int ou float
