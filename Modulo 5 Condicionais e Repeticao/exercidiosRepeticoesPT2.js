877fd5e6a4567f263fd66efa454528eef05fe5d9
Guilherme <50538146+guilhermerek@users.noreply.github.com>
2024-04-18 20:47:34 -0300

    codigo continuacao exercicio

---
+++ Modulo 5 Condicionais e Repeticao/exercidiosRepeticoesPT2.js
@@ -0,0 +1,129 @@
+//Contagem de Itens do Cardápio (Recursivo)
+const cardapio = [
+  'maca',
+  'tomate',
+  'alface',
+  'salada mista',
+  'ovos cozidos unidade',
+];
+
+function contadorRecursivo(lista) {
+  if (lista.length === 0) {
+    return 0;
+  } else {
+    lista.pop();
+    return 1 + contadorRecursivo(lista);
+  }
+}
+
+const res = contadorRecursivo(cardapio);
+console.log(res);
+
+console.log('');
+console.log('ex 2');
+console.log('');
+
+// Cálculo do Valor Total das Vendas (Recursivo)
+
+const arrayTotal = [12, 32, 21, 53, 12, 17, 22];
+const hamburgerPrice = 10;
+
+function totalSelled(arrayTotal, preco, dia) {
+  if (dia >= arrayTotal.length) {
+    return 0;
+  }
+  const valorVendaDiaAtual = arrayTotal[dia] * preco;
+  const valorDoProximoDia = totalSelled(arrayTotal, preco, dia + 1);
+  return valorVendaDiaAtual + valorDoProximoDia;
+}
+
+const resultsEx2 = totalSelled(arrayTotal, hamburgerPrice, 0);
+console.log(resultsEx2);
+
+console.log('');
+console.log('ex 3');
+console.log('');
+
+const menuRestaurant = {
+  maca: 2,
+  tomate: 1,
+  alface: 1,
+  saladaMista: 5,
+  ovosCozidosUnidade: 2,
+  soup: 12,
+  riceWithFries: 8,
+  steak: 13,
+};
+
+function listagemDeItensCardapio(lista) {
+  for (const i in lista) {
+    console.log(`item ${i} : R$${lista[i]}`);
+  }
+}
+
+listagemDeItensCardapio(menuRestaurant);
+
+console.log('');
+console.log('ex 4');
+console.log('');
+
+const cartRestaurant = {
+  soup: 2,
+  riceWithFries: 2,
+  steak: 4,
+};
+
+function calcularTotalConta(lista, listaPedidos) {
+  let total = 0;
+  for (const item in listaPedidos) {
+    if(item in lista) {
+      total += lista[item] * listaPedidos[item];
+    }
+  };
+  return total;
+};
+
+const ex4 = calcularTotalConta(menuRestaurant, cartRestaurant);
+console.log(`O total gasto foi ${ex4}`);
+
+console.log('');
+console.log('ex 5');
+console.log('');
+
+function listagemItensCardápio(lista) {
+  for (i of lista) {
+    console.log(i);
+  }
+}
+
+listagemDeItensCardapio(cardapio);
+
+console.log('');
+console.log('ex 6');
+console.log('');
+
+const cart = [
+  {
+    nome: 'soup',
+    preco: 12,
+  },
+  {
+    nome: 'rice with fries',
+    preco: 8,
+  },
+  {
+    nome: 'steak',
+    preco: 13,
+  },
+];
+
+function calcularTotalConta(lista) {
+  let totalEx6 = 0;
+  for (i of lista) {
+    totalEx6 += lista[i].preco;
+  }
+  return totalEx6;
+}
+
+const ex6 = calcularTotalConta(cart);
+console.log(ex6);
