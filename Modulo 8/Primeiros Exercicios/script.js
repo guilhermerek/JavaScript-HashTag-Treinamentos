67902594c246e912e2e662c5b26275db14f1d559
Guilherme <50538146+guilhermerek@users.noreply.github.com>
2024-06-09 17:42:47 -0300

    exercicios

---
+++ Modulo 8/Primeiros Exercicios/script.js
@@ -0,0 +1,20 @@
+function teste() {
+  console.log('Testando se o script roda!');
+}
+
+function mudarCorTexto() {
+  document.getElementById('mudarCorTextoVermelho').style.color = 'red';
+}
+
+function voltarCor() {
+  document.getElementById('mudarCorTextoVermelho').style.color = 'black';
+}
+
+function alterarImagem() {
+  document.getElementById('alterarImagem').src = 'imagem2.png';
+}
+
+function alterarImagemPadrao() {
+  document.getElementById('alterarImagem').src = 'imagem1.png';
+}
+
