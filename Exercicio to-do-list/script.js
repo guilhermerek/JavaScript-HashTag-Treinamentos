5b205710e6d0a39c87650acbde2474730d52b156
Guilherme <50538146+guilhermerek@users.noreply.github.com>
2024-07-25 11:55:46 -0300

    Finalizacao
    Problema no CSS
    Faltou UL no HTML
    ajustado logica do JS, botao remover não aparecia corretamente e não funcionava, os itens eram adicionados embaixo do botão adicionar também.
    Foi necessário assistir as correções para identificar os problemas.

--- Exercicio to-do-list/script.js
+++ Exercicio to-do-list/script.js
@@ -1,7 +1,27 @@
-function dicionarTarefa(){
+const input = document.getElementById("CampoDeTexto");
+const ul = document.getElementById("taskList");
 
-}
-
-function escurecerBotao() {
+function adicionarTarefa(){
     
+    if (input.value !== null && input.value !== '') {
+        const li = document.createElement("li");
+
+        li.innerText = input.value;
+
+        const botaoRemover = document.createElement("button");
+        botaoRemover.textContent = "Remover";
+        botaoRemover.onclick = function (){
+            ul.removeChild(li);
+        }
+        
+        li.appendChild(botaoRemover);
+        ul.appendChild(li);
+        input.value= null;
+    }
+    else {
+        alert("Campo Vazio!!");
+    }
+
+    //uL.appendChild(lI);
+    //document.getElementById('CampoDeTexto').value= null;
 }
